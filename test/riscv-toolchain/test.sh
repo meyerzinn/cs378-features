b3fc8b830a52ec71c129b92137901acc4a00ff4b
Meyer Zinn <meyerzinn@gmail.com>
2023-08-24 13:26:07 -0500

    bump version and update test

--- test/riscv-toolchain/test.sh
+++ test/riscv-toolchain/test.sh
@@ -3,6 +3,6 @@ set -e
 
 source dev-container-features-test-lib
 
-check "riscv toolchain is present" file /opt/riscv | grep "directory"
+check "riscv toolchain is present" test -f /opt/riscv
 
 reportResults
