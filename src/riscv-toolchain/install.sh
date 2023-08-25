3426cf6f4f385cd7747b84b65b6ffe07535ab10d
Meyer Zinn <meyerzinn@gmail.com>
2023-08-25 12:40:46 -0500

    add riscv toolchain to path

--- src/riscv-toolchain/install.sh
+++ src/riscv-toolchain/install.sh
@@ -1,11 +1,44 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+UPDATE_RC="${UPDATE_RC:-"true"}"
+
 set -e
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+# Ensure that login shells get the correct path if the user updated the PATH using ENV.
+rm -f /etc/profile.d/00-restore-env.sh
+echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" > /etc/profile.d/00-restore-env.sh
+chmod +x /etc/profile.d/00-restore-env.sh
+
+apt_get_update()
+{
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
 
 export DEBIAN_FRONTEND=noninteractive
 
-# install dependencies
-apt-get update -y
-apt-get install -y autoconf automake autotools-dev curl python3 python3-pip libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev ninja-build git cmake libglib2.0-dev
+check_packages autoconf automake autotools-dev curl python3 python3-pip libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev ninja-build git cmake libglib2.0-dev
+if ! type git > /dev/null 2>&1; then
+    check_packages git
+fi
 
 # note: this takes a lot of space
 git clone https://github.com/riscv-collab/riscv-gnu-toolchain.git riscv-gnu-toolchain
@@ -17,3 +50,22 @@ cd riscv-gnu-toolchain
 make -j$(nproc) linux
 
 make -j$(nproc) build-sim SIM=qemu
+
+updaterc() {
+    if [ "${UPDATE_RC}" = "true" ]; then
+        echo "Updating /etc/bash.bashrc and /etc/zsh/zshrc..."
+        if [[ "$(cat /etc/bash.bashrc)" != *"$1"* ]]; then
+            echo -e "$1" >> /etc/bash.bashrc
+        fi
+        if [ -f "/etc/zsh/zshrc" ] && [[ "$(cat /etc/zsh/zshrc)" != *"$1"* ]]; then
+            echo -e "$1" >> /etc/zsh/zshrc
+        fi
+    fi
+}
+
+updaterc "export PATH=$PATH:/opt/riscv/bin"
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+echo "Done!"
